9e463a58365306bd0c76ac29c615186470bc866b
Alex_Prokofev <kuisajke@gmail.com>
2024-04-21 03:23:43 +0300

    я сделал

--- the_snake.py
+++ the_snake.py
@@ -67,7 +67,7 @@ class Apple(GameObject):
 class Snake(GameObject):
     """Конструктор класса змейки"""
 
-    def __init__(self, position=(0,0)):
+    def __init__(self, position=(0, 0)):
         super().__init__(position)
         self.positions = [position]
         self.direction = RIGHT
